366f404404d2a8401c1615a61b89ac5616cde3e4
yellowjin <yellowjin.dev@gmail.com>
2021-10-22 17:07:15 +0900

    Dockerfile user권한 추가

--- build-mrms.sh
+++ build-mrms.sh
@@ -5,6 +5,5 @@ VERSION="1.0.0"
 REGISTRY_URL="registry.seculayer.com:31500"
 
 # docker build
-# DOCKER_BUILDKIT=1 docker build -t registry.seculayer.com:31500/ape/automl-mrms:1.0.0 .
 DOCKER_BUILDKIT=1 docker build -t $REGISTRY_URL/ape/automl-mrms:$VERSION .
 docker push $REGISTRY_URL/ape/automl-mrms:$VERSION
